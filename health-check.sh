fd8b6aa9fb1c579874ac98e75105f984d2a53720
mewc <m@mewc.info>
2023-08-01 21:02:23 +1000

    iupdate checking branch

--- health-check.sh
+++ health-check.sh
@@ -3,7 +3,7 @@
 # later upstream merges messy for anyone who forked us.
 commit=true
 origin=$(git remote get-url origin)
-if [[ $origin == *statsig-io/statuspage* ]]
+if [[ $origin == *wearetrendii/trendiistatus* ]]
 then
   commit=false
 fi
